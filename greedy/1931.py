88b0037acfd504d4e523a765cd0dc3af26216533
Namwoosung <142873609+Namwoosung@users.noreply.github.com>
2024-01-18 11:21:54 +0900

    24-01-18

--- greedy/1931.py
+++ greedy/1931.py
@@ -9,26 +9,52 @@ meets = list()
 for i in range(num):
     meet = list(map(int, input().split()))
     meets.append(meet)
+#종료시간 기준 정렬, 같은 종료시간 내에서는 빠른 시작시간 순으로 정렬 <- 시작시간과 종료시간이 동일한 회의 처리를 위해 필요
+meets.sort(key=lambda x:x[0])
 meets.sort(key=lambda x:x[1])
 
 #set count
 count = [0] * (meets[num-1][1] + 1)
 
-
-index, i = 0, 0
-while i < len(count):
-    #현재 check할 회의가 없으면 이전 시간과 동일
-    if i != meets[index][1]:
-        count[i] = count[i - 1]
-        i += 1
-    #check할 회의가 존재하면, 현재 회의를 넣는 경우와 아닌 경우를 고려해 더 큰 값을 count에 저장
-    else:
-        if count[i - 1] > (count[meets[index][0]] + 1):
-            count[i] = count[i - 1]
-        else:
-            count[i] = count[meets[index][0]] + 1
-        
+index = 0
+#각 회의마다 해당 회의를 포함하는 것이 좋은 지 아닌 지 check
+for item in meets:
+    #이전 시간들을 update(일단 현재 회의를 포함하지 않는다고 가정하고 값 대입)
+    while index < item[1]:
+        count[index + 1] = count[index]
         index += 1
+    #현재 회의를 포함했을 때 더 많은 회의를 가질 수 있으면 해당 값으로 설정
+    if count[index] < (count[item[0]] + 1):
+        count[index] = count[item[0]] + 1
+
+print(count[-1])
 
 
-print(count[len(count) - 1])
+
+"""
+더 깔끔한 코드
+내 풀이는 DP느낌으로 접근 아래 풀이는 greedy로 접근
+그냥 시작시간 및 종료시간으로 정렬하고
+현재 검사하는 회의의 시작시간이 이전 종료시간 이상이면 회의를 하는 것으로 진행
+import sys
+
+N = int(sys.stdin.readline())
+
+time = [[0]*2 for _ in range(N)]
+for i in range(N):
+    s, e = map(int, sys.stdin.readline().split())
+    time[i][0] = s
+    time[i][1] = e
+
+time.sort(key = lambda x: (x[1], x[0]))
+
+cnt = 1
+end_time = time[0][1]
+for i in range(1, N):
+    if time[i][0] >= end_time:
+        cnt += 1
+        end_time = time[i][1]
+
+print(cnt)
+
+"""
--- greedy/21758.cpp
+++ greedy/21758.cpp
@@ -14,21 +14,19 @@ int main() {
 	}
 
 
-	//set maxIndex
+	//set maxIndex for 꿀통이 중간에 존재하는 경우
 	int maxIndex = 1;
 	for (int i = 1; i < N - 1; i++) {
 		if (honey[maxIndex] < honey[i]) maxIndex = i;
 	}
-
 	int maxHoney = 0;
 	//case 1. 꿀통 중간, 벌 양쪽 끝
 	for (int i = 1; i < N - 1; i++) {
 		maxHoney += honey[i];
-		if (maxIndex == i) maxHoney += honey[i];
 	}
+	maxHoney += honey[maxIndex];
 
 	//case 2.꿀통 왼쪽
-
 	//두 번째 벌의 index를 탐색
 	int beeIndex = N - 2;
 	while (beeIndex > 1) {
@@ -44,11 +42,10 @@ int main() {
 		if (i < beeIndex) temp += honey[i] * 2;
 		else if (i > beeIndex) temp += honey[i];
 	}
-
 	if (temp > maxHoney) maxHoney = temp;
 
 	//case 3.꿀통 오른쪽
-		//두 번째 벌의 index를 탐색
+	//두 번째 벌의 index를 탐색
 	beeIndex = 1;
 	while (beeIndex < N - 2) {
 		if (honey[beeIndex] >= honey[beeIndex + 1] * 2) {
@@ -59,11 +56,10 @@ int main() {
 		}
 	}
 	temp = 0;
-	for (int i = 0; i < N - 1; i++) {
+	for (int i = 1; i < N; i++) {
 		if (i < beeIndex) temp += honey[i];
 		else if (i > beeIndex) temp += honey[i] * 2;
 	}
-
 	if (temp > maxHoney) maxHoney = temp;
 
 	cout << maxHoney;
@@ -72,3 +68,43 @@ int main() {
 
 	return 0;
 }
+
+
+/*
+접근법은 동일 3가지 case로 나눠서 진행
+코드가 더 깔끔함
+차이점
+1. 여기서는 전체 꿀의 합을 더하고 이후에 빼주면서 계산
+2. 나는 2번째 벌의 위치를 수학적으로 찾았다면, 여기서는 모든 경우를 탐색하고 max값을 취함
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+long long v[100001], sum[100001];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	cin >> n;
+	for(int i=1; i<=n; i++){
+		cin >> v[i];
+		sum[i]=v[i]+sum[i-1];
+	}
+	long long ans=0;
+	for(int i=2; i<n; i++){ // i)
+		ans=max(ans, sum[n]-v[1]-v[i]+sum[n]-sum[i]);
+	}
+	for(int i=2; i<n; i++){ // ii)
+		ans=max(ans, sum[n]-v[n]-v[i]+sum[i-1]);
+	}
+	for(int i=2; i<n; i++){ // iii)
+		ans=max(ans, sum[i]-v[1]+sum[n]-sum[i-1]-v[n]);
+	}
+	cout << ans << "\n";
+	return 0;
+}
+*/
